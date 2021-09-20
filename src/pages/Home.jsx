91f9b6d56aab2b57f25c7615a5ead03aab8a4246
Paulo França <paulovinicius.franca@gmail.com>
2021-09-20 14:08:23 -0300

    Requisito 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import shoppingCartIcon from '../images/shoppingCartIcon.png';
 
 class Home extends Component {
   render() {
@@ -7,6 +9,13 @@ class Home extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link data-testid="shopping-cart-button" to="/shoppingcart">
+          <img
+            className="shopping-cart-img"
+            src={ shoppingCartIcon }
+            alt="shoppingCartIcon"
+          />
+        </Link>
       </section>
     );
   }
---
+++ src/pages/ShoppingCart.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+  render() {
+    return (
+      <section>
+        <p data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
+        </p>
+      </section>
+    );
+  }
+}
+
+export default ShoppingCart;
