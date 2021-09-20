78633f366ed63a9dd3d056762b1deccdb14d0068
Elithon Silva <elithonsilva@gmail.com>
2021-09-20 16:41:45 -0300

    faz chamada da API Category e rederiza no componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      getCategoryAPI: [],
+      requestAPI: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getProductCategoryAPI();
+  }
+
+  getProductCategoryAPI = async () => {
+    const getAPI = await getCategories();
+    this.setState({
+      getCategoryAPI: getAPI,
+      requestAPI: true,
+    });
+  }
+
   render() {
+    const { getCategoryAPI, requestAPI } = this.state;
     return (
       <section>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        {requestAPI
+          ? (
+            <ul>
+              {getCategoryAPI.map((category) => (
+                <li key={ category.id } data-testid="category">{ category.name }</li>))}
+            </ul>)
+          : ''}
       </section>
     );
   }
