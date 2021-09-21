3403ed02b943acd9438da9771818d8abe184adb7
Bruno <brunocostah@hotmail.com>
2021-09-20 21:31:34 -0300

    Requisito 05

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import shoppingCartIcon from '../images/shoppingCartIcon.png';
+import ProductCatalog from '../components/ProductCatalog';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      productCatalog: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.searchProducts = this.searchProducts.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  /**
+ * Consultei o repositório do Grupo 29 para resolver essa parte do SearchProducts().
+ * https://github.com/tryber/sd-014-b-project-frontend-online-store/pull/157
+ */
+  async searchProducts() {
+    const { searchText } = this.state;
+    const result = await getProductsFromCategoryAndQuery('', searchText);
+    this.setState({
+      productCatalog: result.results,
+    });
+    return result;
+  }
+
   render() {
+    const { productCatalog, searchText } = this.state;
+
     return (
       <section>
         <p data-testid="home-initial-message">
@@ -16,6 +51,17 @@ class Home extends Component {
             alt="shoppingCartIcon"
           />
         </Link>
+        <input
+          type="text"
+          data-testid="query-input"
+          name="searchText"
+          value={ searchText }
+          onChange={ this.handleChange }
+        />
+        <ProductCatalog productCatalog={ productCatalog } />
+        <button type="button" data-testid="query-button" onClick={ this.searchProducts }>
+          Pesquisa
+        </button>
       </section>
     );
   }
